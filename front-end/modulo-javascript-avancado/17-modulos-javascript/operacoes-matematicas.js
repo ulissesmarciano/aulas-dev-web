2a7349ede10e85ceaaf8f7c7f9cc5058365fe52e
Ulisses <ulissesmkt2104@gmail.com>
2024-12-06 14:33:32 -0300

    Atualização: adiconando alternativa de código

--- front-end/modulo-javascript-avancado/17-modulos-javascript/operacoes-matematicas.js
+++ front-end/modulo-javascript-avancado/17-modulos-javascript/operacoes-matematicas.js
@@ -1,4 +1,8 @@
 export function somar(num1, num2) {
     return num1 + num2
 }
+
+export function multiplicar(num1, num2) {
+    return num1 * num2
+}
 // ou  export { somar, multiplicar } quando nao utilizar o export function.
